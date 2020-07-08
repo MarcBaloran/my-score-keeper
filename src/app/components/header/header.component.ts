d510214e79c3b2e4d74945fb60d7f2cd746ed65b
Marc <marcbaloran@hotmail.com>
2020-07-08 15:03:14 +0100

    Add possible future logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { min } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,45 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   constructor() { }
+  allplayers:{team: string, score: number}[];
+  scores: {[key:string]: number}
 
   ngOnInit(): void {
+    
+    // this.allplayers.reduce((score,currPlayer) => ({
+    //   ...score,
+    //   [score[currPlayer.team]]: (score[currPlayer.team] || 0) + currPlayer.score 
+    // }), {});
+
+
+    // const allPlayersInTeam1: {team: string, score: number}[];
+
+    // allPlayersInTeam1.reduce((accumulator, currPlayer) => accumulator + currPlayer.score, 0)
   }
+
+
+
+    /*
+{
+  team1: 46,
+  team2: 46
+}
+
+//before anything executes
+{
+
+}
+
+after player1 { }
+{
+  team1 : 1
+}
+
+
+    */
+
+
+
+
 
 }
